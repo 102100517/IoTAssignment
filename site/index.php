01ed916fd20d01b2ba7388d5198430ec70c43dea
102100517 <102100517@student.swin.edu.au>
2019-05-11 13:22:06 +1000

    adding index.php

---
+++ site/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IoT Personal Assignment</title>
+	<link rel="stylesheet" href="./css/bootstrap.css">
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-expand navbar-dark bg-dark sticky-top">
+		<a href="index.php" class="navbar-brand">IoT Personal Assignment</a>
+		</nav>
+	</header>
+	
+	<main class="container lead">
+		<div class="row">
+			<h1 class="display-1">Welcome!</h1>
+		</div>
+		
+		<div class="row">
+			<p>
+			The current personal project that is the product of many man-hours<br />
+			Developed by: James Hassall
+			</p>
+		</div>
+	</main>
+</body>
+</html>
