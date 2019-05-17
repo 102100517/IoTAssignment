fdcca95eabe45f32c5d564ac61c98cf1528de071
102100517 <102100517@student.swin.edu.au>
2019-05-17 17:27:37 +1000

    adding obd functionality

--- arduino/obd2-logger/obd2-logger.ino
+++ arduino/obd2-logger/obd2-logger.ino
@@ -1,9 +1,34 @@
+#include <OBD2UART.h>
+#include <ArduinoJson.h>
+#include <RTClib.h>
+
+int ledpin=13;
+const int capacity = JSON_OBJECT_SIZE(2);
+StaticJsonDocument<capacity> doc;
+COBD obd;
+int RPMspeed = 0;
+int EngineLoad = 0;
+//RTC_DS1307 RTC;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(ledpin,OUTPUT);
+  Serial.begin(9600);
+  while(!Serial){
+    ;
+  }
+  obd.begin();
+  while (!obd.init()); 
+  //RTC.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  doc["Action"].set("Read");
+  obd.readPID(PID_RPM, RPMspeed);
+  doc["RPM"].set(RPMspeed);
+  obd.readPID(PID_ENGINE_LOAD, EngineLoad);
+  doc["Engine Load"].set(EngineLoad);
+  JsonObject obj = doc.to<JsonObject>();
+  serializeJsonPretty(doc,Serial);
 }
