4db3b7c9f951a5b52362d47fea77ef10a1bf3e02
102100517 <102100517@student.swin.edu.au>
2019-05-27 20:43:51 +1000

    site updating

--- site/index.php
+++ site/index.php
@@ -8,6 +8,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 <body>
+	<?php	
+		$conn = new MongoClient("mongodb://localhost:27017");
+		$db = $conn->mydb;
+		$collection $db->smartcar;
+
+		$document = $collection->findOne();
+
+		var_dump($document);
+	?>
 	<header>
 		<nav class="navbar navbar-expand navbar-dark bg-dark sticky-top">
 		<a href="index.php" class="navbar-brand">IoT Personal Assignment</a>
